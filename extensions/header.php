4703a4e2a8096d9c758f3a7d46683873bf270e47
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-02-06 12:12:32 +0100

    Ajout Username et Bouton déconnexion si utilisateur connecté

--- extensions/header.php
+++ extensions/header.php
@@ -1,6 +1,22 @@
 <?php 
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 ?>
-<div class="header"><img src="img/gbaf.png" alt="Logo GBAF" id="logo">
-<div class="nom_compte"><?php //echo htmlspecialchars($_SESSION['username']);//Ajout de l'username ?></div>
+<div class="header">
+  <img src="img/gbaf.png" alt="Logo GBAF" id="logo">
+  <div class="nom_compte ">  
+    <?php
+    if(isset($_SESSION['utilisateur'])) {
+        echo $_SESSION['utilisateur'];
+        echo '<form method="post" action="functions/deconnexion.php">';
+        echo '<input type="submit" name="deconnexion" value="Déconnexion">';
+        echo '</form>';
+    } else {
+        
+    }
+    ?> 
+  </div>
 </div>
