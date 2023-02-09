685470df7678e4ba1c6de4226b07bb4696a165e1
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-02-09 18:03:18 +0100

    Récuperation id_user

--- connexion_traitement.php
+++ connexion_traitement.php
@@ -1,6 +1,6 @@
 <?php
 require('config.php');
-$check = $conn->prepare("SELECT username, password FROM account WHERE username=?");
+$check = $conn->prepare("SELECT id_user, username, password FROM account WHERE username=?");
 $result = $check->execute(array($_POST['username']));
 if (!$result) {
     echo 'Erreur lors de l\'exécution de la requête : ' . print_r($check->errorInfo(), true);
@@ -9,6 +9,7 @@ if (!$result) {
     if ($compte_user && password_verify($_POST['password'], $compte_user['password'])) {
         session_start();
         $_SESSION['utilisateur'] = $_POST['username'];
+        $_SESSION['id_user'] = $compte_user['id_user'];
         header('Location:/acceuil.php');
     } else {
         echo "Nom d'utilisateur ou mot de passe incorrect <br> <a href='index.php'>Retour à la page d'accueil</a>";
