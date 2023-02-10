8cf1f1ffbfb7934ee371fe10434811235564a3bf
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-02-10 13:24:19 +0100

    Ajout fonctionnalité vote + BDD

--- organisme.php
+++ organisme.php
@@ -12,79 +12,97 @@
     <?php
     require_once('functions/auth.php');
     require_once('config.php');
-    include('extensions/header.php')
-    ?>
-    <?php
+    include('extensions/header.php');
+    function is_logged_in()
+    {
+        return isset($_SESSION['id_user']);
+    }
 
+    // Récupération de l'ID de l'acteur passé en paramètre dans l'URL
     $id_acteur = isset($_GET['id']) ? $_GET['id'] : 0;
 
+    // Récupération des informations de l'acteur dans la base de données
     $stmt = $conn->prepare("SELECT * FROM acteur WHERE id_acteur = :id");
-
     $stmt->bindValue(':id', $id_acteur);
-
     $stmt->execute();
-
     $acteur = $stmt->fetch(PDO::FETCH_ASSOC);
 
-   
+    // Si l'acteur a été trouvé
     if ($acteur) {
-        $stmt = $conn->prepare("SELECT COUNT(*) FROM post WHERE id_acteur = :id");
-        $stmt->bindValue(':id', $id_acteur);
+        // Récupération du nombre de commentaires pour l'acteur
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM post WHERE id_acteur = :id_acteur");
+        $stmt->bindValue(':id_acteur', $id_acteur);
         $stmt->execute();
         $comment_count = $stmt->fetchColumn();
-        
+    };
+    $id_user = $_SESSION['id_user'];
+    // Affichage des informations de l'acteur et des boutons de vote et commentaires
     ?>
-    
-        <div class="element2">
-            <div class="divimg2">
-                <img class="img_acteur" src="<?php echo $acteur['logo']; ?>" alt="Logo <?php echo $acteur['acteur']; ?>">
-            </div>
-            <div class="acteurtxt2">
-                <h3><?php echo $acteur['acteur']; ?></h3>
-                <p><?php echo $acteur['description']; ?></p>
-            </div>
+    <div class="element2">
+        <div class="divimg2">
+            <img class="img_acteur" src="<?php echo $acteur['logo']; ?>" alt="Logo <?php echo $acteur['acteur']; ?>">
         </div>
-        <div class="comments">
-            <?php echo $comment_count . " " . "Commentaires"?>
-            <form action="comments_traitement.php" method="post">
+        <div class="acteurtxt2">
+            <h3><?php echo $acteur['acteur']; ?></h3>
+            <p><?php echo $acteur['description']; ?></p>
+        </div>
+    </div>
+    <div class="comments">
+        <?php echo $comment_count . " " . "Commentaires"; ?>
+        <div class="groupe_btn">
+        <form action="comments_traitement.php" method="post">
+            <div class="zone_commentaire">
             <input type="hidden" name="id_acteur" value="<?php echo $id_acteur; ?>">
-            <input type="text" name="post" required><br>
-                <div class ="groupe_btn">
-                <button class="like_btn"><i class="fa fa-thumbs-up"></i></button>
-                <button class="dislike_btn"><i class="fa fa-thumbs-down"></i></button>
-                <a class="comments-btn" href="comments_traitement.php?id_acteur=<?php echo $id_acteur; ?>"><button type="submit" name="button_comments" > Ajouter un commentaire</button></a>
-        </div></form>
-    <?php
-    }
-    ?>
-<?php
-$stmt = $conn->prepare("SELECT * FROM post LEFT JOIN account ON post.id_user = account.id_user WHERE post.id_acteur = :id_acteur ORDER BY date_add DESC LIMIT 3");
-$stmt->bindParam(':id_acteur', $id_acteur, PDO::PARAM_INT);
-$stmt->execute();
-$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if ($posts) {
-    foreach ($posts as $post) {
-        ?>
-        <div class="">
-            <div class="prenom_comment">
-                <?php echo $post['prenom']; ?>
-            </div>
-            <div class="date_comment">
-                <?php echo $post['date_add']; ?>
-            </div>
-            <div class="post_comment">
-                <?php echo $post['post']; ?>
-            </div>
-        </div>
+            <input placeholder="Nouveau commentaire" type="text" name="post" required><br> </div>
+                <a class="comments-btn" href="comments_traitement.php?id_acteur=<?php echo $id_acteur; ?>">
+                    <button type="submit" name="button_comments">Ajouter un commentaire</button>
+                </a>
+
+        </form>
         <?php
-    }
-} else {
-    echo 'Aucun commentaire';
-}
+        if (is_logged_in()) {
+        ?>
+            <form action="vote_traitement.php" method="post">
+                <input type="hidden" name="id_acteur" value="<?php echo $id_acteur; ?>">
+                <input type="hidden" name="id_user" value="<?php echo $id_user; ?>"></button>
+                <button class="like_btn" type="submit" name="vote" value="1">
+                    <i id="id_like" class="fa fa-thumbs-up"></i>
+                </button>
+                <button class="dislike_btn" type="submit" name="vote" value="-1">
+                    <i id="dislike_btn" class="fa fa-thumbs-down"></i>
+                </button>
+            </form></div>
+
+        <?php
+        }
+        ?>
+        <?php
+        $stmt = $conn->prepare("SELECT * FROM post LEFT JOIN account ON post.id_user = account.id_user WHERE post.id_acteur = :id_acteur ORDER BY date_add DESC LIMIT 3");
+        $stmt->bindParam(':id_acteur', $id_acteur, PDO::PARAM_INT);
+        $stmt->execute();
+        $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($posts) {
+            foreach ($posts as $post) {
+        ?>
+                <div class="afficher_commentaire">
+                    <div class="prenom_comment">
+                        <?php echo $post['prenom']; ?>
+                    </div>
+                    <div class="date_comment">
+                        <?php echo $post['date_add']; ?>
+                    </div>
+                    <div class="post_comment">
+                        <?php echo $post['post']; ?>
+                    </div>
+                </div>
+   
+<?php
+            }
+        } else {
+            echo 'Aucun commentaire';
+        }
 ?>
-        </div>
-
+ </div>
 </body>
-
 </html>
 <?php include('extensions/footer.php'); ?>
---
+++ vote_traitement.php
@@ -0,0 +1,36 @@
+<?php
+require_once('config.php');
+require_once('functions/auth.php');
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=UTF8",$username, $password);
+  } catch (PDOException $e) {
+    die('Erreur : ' . $e->getMessage());
+  }
+// Récupération des données du formulaire
+$id_user = $_SESSION['id_user'];
+$id_acteur = $_POST['id_acteur'];
+$vote = $_POST['vote'];
+
+
+// Vérification si l'utilisateur a déjà voté pour l'acteur
+$stmt = $bdd->prepare('SELECT * FROM vote WHERE id_user = :id_user AND id_acteur = :id_acteur');
+$stmt->execute(array(
+  ':id_user' => $id_user,
+  ':id_acteur' => $id_acteur
+));
+$result = $stmt->fetch();
+
+if ($result) {
+  // Affichage d'un message d'erreur si l'utilisateur a déjà voté
+  echo 'Vous avez déjà voté pour cet acteur.';
+} else {
+  // Enregistrement du nouveau vote
+  $stmt = $bdd->prepare('INSERT INTO vote (id_user, id_acteur, vote) VALUES (:id_user, :id_acteur, :vote)');
+  $stmt->execute(array(
+    ':id_user' => $id_user,
+    ':id_acteur' => $id_acteur,
+    ':vote' => $vote
+  ));
+}
+  // Mise à jour du nombre de votes
