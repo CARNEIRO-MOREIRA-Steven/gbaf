12e25841f661e00779f5905f70fc0ec62877c579
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-02-15 12:39:20 +0100

    Vérification html

--- parametre_compte.php
+++ parametre_compte.php
@@ -16,16 +16,16 @@ require('functions/auth.php')
     <form method="post" action="parametre_compte_traitement.php">
         <fieldset>
         <legend>Vos Coordonnées</legend>
-        <label>Nom</label> : <input type="text" name="nom" id="nom"required><br>
-        <label>Prenom</label> : <input type="text" name="prenom" id="prenom"required><br>
-        <label>Password</label> : <input type="password" name="password" id="password"required><br>
+        <label>Nom</label> : <input type="text" name="nom" id="nom" required><br>
+        <label>Prenom</label> : <input type="text" name="prenom" id="prenom" required><br>
+        <label>Password</label> : <input type="password" name="password" id="password" required><br>
         <label>Question secrète</label> : 
 <select name="question" id="question-select">
 	<option value="père">Nom de votre père</option>
 	<option value="animal">Nom de votre animal de compagnie</option>
 	<option value="ville">Ville de naissance</option>
-</select><br/>
-        <label>Réponse</label> : <input type="text" name="reponse" id="reponse"required<br></p>
+</select><br>
+        <label>Réponse</label> : <input type="text" name="reponse" id="reponse" required><br>
         <input type="submit" value="Modifier"> 
         <button id="retour" onclick="window.location.href='acceuil.php'">Retour</button>
     </fieldset>
